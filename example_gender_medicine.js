6e88141c83a23fccc0c4d006578851d9c3e3122e
EmmaKHarrington <113444544+EmmaKHarrington@users.noreply.github.com>
2022-09-19 08:41:30 -0500

    Create example_gender_medicine.js

---
+++ example_gender_medicine.js
@@ -0,0 +1,98 @@
+define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/IAT/qualtrics/quiat9.js'], function(APIConstructor, iatExtension){
+    var API = new APIConstructor();
+
+	define(['pipAPI','https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/IAT/qualtrics/quiat9.js'], function(APIConstructor, iatExtension){
+	var API = new APIConstructor();
+
+return iatExtension({
+      attribute1 : {
+			name : 'Male', //Will appear in the data.
+			title : {
+				media : {word : 'Male'}, //Name of the category presented in the task.
+				css : {color:'#0000FF','font-size':'1.8em'}, //Style of the category title.
+				height : 4 //Used to position the "Or" in the combined block.
+			}, 
+			stimulusMedia : [ //Stimuli content as PIP's media objects
+				{word: 'Man'},
+				{word: 'Son'},
+				{word: 'Father'},
+				{word: 'Boy'},
+				{word: 'Uncle'},
+				{word: 'Grandpa'},
+				{word: 'Husband'},
+				{word: 'Male'}	
+			], 
+			//Stimulus css (style)
+			stimulusCss : {color:'#0000FF','font-size':'2.3em'}
+		},	
+		attribute2 :	{
+			name : 'Female', //Will appear in the data.
+			title : {
+				media : {word : 'Female'}, //Name of the category presented in the task.
+				css : {color:'#0000FF','font-size':'1.8em'}, //Style of the category title.
+				height : 4 //Used to position the "Or" in the combined block.
+			}, 
+			stimulusMedia : [ //Stimuli content as PIP's media objects
+				{word: 'Mother'},
+				{word: 'Wife'},
+				{word: 'Aunt'},
+				{word: 'Woman'},
+				{word: 'Girl'},
+				{word: 'Female'},
+				{word: 'Grandma'},
+				{word: 'Daughter'}
+			], 
+			//Stimulus css
+			stimulusCss : {color:'#0000FF','font-size':'2.3em'}
+		},
+		category1 : 
+		{
+			name : 'Teaching', 
+			title : {
+				media : {word : 'Teaching'}, 
+				css : {color:'#31940F','font-size':'1.8em'}, 
+				height : 4 //Used to position the "Or" in the combined block.
+			}, 
+			stimulusMedia : [ //Stimuli content as PIP's media objects
+				{word: 'Kindergarten'},
+				{word: 'HS'},
+				{word: 'Principal'},
+				{word: 'Professor'},
+				{word: 'MS'},
+				{word: 'PreK'},
+				{word: 'Childcare'}
+			], 
+			//Stimulus css
+			stimulusCss : {color:'#31940F','font-size':'2.3em'} 
+		},
+		category2 : 
+		{
+			name : 'Medicine', 
+			title : {
+				media : {word : 'Medicine'}, 
+				css : {color:'#31940F','font-size':'1.8em'}, 
+				height : 4 //Used to position the "Or" in the combined block.
+			}, 
+			stimulusMedia : [ //Stimuli content as PIP's media objects
+				{word: 'Nurse'},
+				{word: 'Doctor'},
+				{word: 'PA'},
+				{word: 'EMT'},
+				{word: 'RN'},
+				{word: 'Surgeon'}, 
+				{word: 'Aide'}
+			], 
+			//Stimulus css
+			stimulusCss : {color:'#31940F','font-size':'2.3em'}
+		},
+		
+		//The default feedback messages for each cutoff -
+			//attribute1, and attribute2 will be replaced with the name of attribute1 and attribute2.
+			//categoryA is the name of the category that is found to be associated with attribute1,
+			//and categoryB is the name of the category that is found to be associated with attribute2.
+			fb_strong_Att1WithCatA_Att2WithCatB : 'Your data suggest a strong automatic association for attribute1 with categoryA and attribute2 with categoryB.',
+			fb_moderate_Att1WithCatA_Att2WithCatB : 'Your data suggest a moderate automatic association for attribute1 with categoryA and attribute2 with categoryB.',
+			fb_slight_Att1WithCatA_Att2WithCatB : 'Your data suggest a slight automatic association for attribute1 with categoryA and attribute2 with categoryB.',
+			fb_equal_CatAvsCatB : 'Your data suggest little or no automatic association between attribute2 and attribute1 with categoryA and categoryB..',
+    });
+});
